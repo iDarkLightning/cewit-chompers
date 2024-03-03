f623d242877f7c0e8a4f03a4d8bb0a400a2d52d2
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-03-03 07:21:03 -0500

    table management

---
+++ client/src/components/restaurant/create-table.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/16/solid";
+import { useForm } from "@tanstack/react-form";
+import { getRouteApi, useNavigate } from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-form-adapter";
+
+import { trpc } from "~/client/utils/trpc";
+import { Button } from "../ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
+import { Input } from "../ui/input";
+
+const routeApi = getRouteApi("/restaurant/$slug/manage/tables");
+
+const CreateTableForm = () => {
+  const params = routeApi.useParams();
+
+  const navigate = useNavigate();
+  const utils = trpc.useUtils();
+
+  const create = trpc.restaurant.table.create.useMutation({
+    onSuccess: (data) => {
+      // utils.restaurant.table.
+    },
+  });
+
+  const form = useForm({
+    defaultValues: {
+      seats: "",
+    },
+    onSubmit: async (values) => {
+      create.mutate({
+        seats: parseInt(values.value.seats),
+        restaurantSlug: params.slug,
+      });
+    },
+    validatorAdapter: zodValidator,
+  });
+
+  return (
+    <form.Provider>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          form.handleSubmit();
+        }}
+        className="flex flex-col gap-8"
+      >
+        <form.Field name="seats">
+          {(field) => (
+            <fieldset className="flex flex-col gap-1">
+              <label
+                htmlFor={field.name}
+                className="text-sm font-medium text-neutral-800"
+              >
+                Seats
+              </label>
+              <Input
+                type="number"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            </fieldset>
+          )}
+        </form.Field>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button type="submit" disabled={!canSubmit} className="w-full">
+              {isSubmitting ? "..." : "Add"}
+            </Button>
+          )}
+        />
+      </form>
+    </form.Provider>
+  );
+};
+
+export const CreateTableDrawer = () => {
+  const [open, onOpenChange] = useState(false);
+
+  return (
+    <>
+      <Button variant="secondary" onClick={() => onOpenChange(true)}>
+        <span className="mr-4">
+          <PlusIcon className="h-4 w-4" />
+        </span>
+        <span>Add Table</span>
+      </Button>
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent className="px-8 py-4">
+          <DrawerHeader>
+            <DrawerTitle>Create Table</DrawerTitle>
+            <DrawerDescription>
+              Create a new table for this restaurant
+            </DrawerDescription>
+          </DrawerHeader>
+          <CreateTableForm />
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
--- client/src/routes/restaurant.$slug.manage.tables.lazy.tsx
+++ client/src/routes/restaurant.$slug.manage.tables.lazy.tsx
@@ -1,11 +1,116 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { QrCodeIcon, XMarkIcon } from "@heroicons/react/16/solid";
+import { createLazyFileRoute, Link } from "@tanstack/react-router";
+import Code from "react-qr-code";
 
-export const Route = createLazyFileRoute(
-  "/restaurant/$slug/manage/tables",
-)({
+import { trpc } from "~/client/utils/trpc";
+import { CreateTableDrawer } from "../components/restaurant/create-table";
+import { Button } from "../components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../components/ui/drawer";
+
+export const Route = createLazyFileRoute("/restaurant/$slug/manage/tables")({
   component: TablesView,
 });
 
 function TablesView() {
-  return <></>;
+  const params = Route.useParams();
+  const [restaurant] = trpc.restaurant.getBySlug.useSuspenseQuery({
+    restaurantSlug: params.slug,
+  });
+  const utils = trpc.useUtils();
+
+  const clearTable = trpc.restaurant.table.clear.useMutation({
+    onSuccess: async () => {
+      await utils.restaurant.getBySlug.invalidate();
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between gap-4 px-6">
+        <h2 className="text-xl font-medium">Table</h2>
+        <CreateTableDrawer />
+      </div>
+
+      <div className="flex flex-col gap-4 px-6">
+        {restaurant.tables.map((table, idx) => (
+          <div
+            key={table.id}
+            className="rounded-lg border-[0.0125rem] border-neutral-300 p-4"
+          >
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="text-lg font-medium">Table {idx + 1}</p>
+                <p>Seats: {table.seats}</p>
+              </div>
+              {table.occupied && (
+                <p className="my-2 rounded-full bg-amber-200 px-4 py-0.5 font-medium">
+                  Occupied
+                </p>
+              )}
+              {!table.occupied && (
+                <p className="my-2 rounded-full bg-teal-200 px-4 py-0.5 font-medium">
+                  Not Occupied
+                </p>
+              )}
+            </div>
+            <div className="mt-4 flex gap-2">
+              <Drawer>
+                <DrawerTrigger asChild>
+                  <Button variant="secondary" className="inline-flex gap-2">
+                    <span>
+                      <QrCodeIcon className="h-4 w-4" />
+                    </span>
+                    <span>QR Code</span>
+                  </Button>
+                </DrawerTrigger>
+                <DrawerContent className="px-8 py-4">
+                  <DrawerHeader>
+                    <DrawerTitle>QR Code</DrawerTitle>
+                    <DrawerDescription>
+                      Scan this code to be seated at this table
+                    </DrawerDescription>
+                  </DrawerHeader>
+                  <div className="flex items-center justify-center">
+                    <Code
+                      width={128}
+                      value={window.location.origin + `/order/${table.id}`}
+                      bgColor="#fff"
+                      fgColor="#1c1c1c"
+                    />
+                  </div>
+                  <Button className="my-4" asChild>
+                    <Link to="/order/$id" params={{ id: table.id }}>
+                      Go to Order View
+                    </Link>
+                  </Button>
+                </DrawerContent>
+              </Drawer>
+              <Button variant="secondary" className="inline-flex gap-2">
+                <span>
+                  <XMarkIcon className="h-4 w-4" />
+                </span>
+                <span
+                  onClick={() => {
+                    clearTable.mutate({
+                      tableId: table.id,
+                      restaurantSlug: restaurant.slug,
+                    });
+                  }}
+                >
+                  Clear Table
+                </span>
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
