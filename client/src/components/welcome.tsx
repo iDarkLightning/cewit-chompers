c78e4977a1f1735989b11b3370eeb9a47c919495
awareermine <aramie.ewen@gmail.com>
2024-03-03 05:39:29 -0500

    start onboarding list

---
+++ client/src/components/welcome.tsx
@@ -0,0 +1,56 @@
+import Checkbox from "./ui/checkbox";
+
+export const Welcome = () => {
+  const commonIngredients = [
+    "chicken",
+    "beef",
+    "potatoes",
+    "rice",
+    "pasta",
+    "cilantro",
+    "flour",
+    "cheese",
+  ];
+
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <h1 className="animate-fade-right text-4xl font-semibold">
+          Welcome to Choosy Chompers
+        </h1>
+        <p className="animate-fade-up text-xl text-neutral-600">
+          Choose the foods you'd like to eat.
+        </p>
+        <video
+          id="video"
+          // loop
+          autoplay=""
+          muted
+          className="block rounded-xl shadow-lg"
+        >
+          <source src="/public/chompers.mp4" type="video/mp4" />
+        </video>
+        <p>We'll assume you don't like the ones you don't select</p>
+        <div>
+          {commonIngredients.map((i) => (
+            <Checkbox label={i} key={i} id={i} />
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            const liked: string[] = commonIngredients.filter(
+              (i) => document.getElementById(i)?.checked,
+            );
+            const disliked: string[] = commonIngredients.filter(
+              (i) => !document.getElementById(i)?.checked,
+            );
+            alert(liked);
+            alert(disliked);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </>
+  );
+};
