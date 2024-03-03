bb54e1d1b1405629e87da9d7d93315706f2a4072
awareermine <aramie.ewen@gmail.com>
2024-03-03 04:26:42 -0500

    add likes and dislikes

--- server/api/trpc/routes/user-router.ts
+++ server/api/trpc/routes/user-router.ts
@@ -1,29 +1,29 @@
 import { TRPCError } from "@trpc/server";
-import { router } from "..";
-import { authedProcedure } from "../../auth/authed-procedure";
 import { z } from "zod";
 
-export const customerProcedure = authedProcedure
-  .use(async (opts) => {
-    const customer = await opts.ctx.prisma.customer.findUnique({
-      where: {
-        userId: opts.ctx.user.id
-      }
-    });
+import { router } from "..";
+import { authedProcedure } from "../../auth/authed-procedure";
 
-    if (!customer) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "No order found"
-      });
-    }
-
-    return opts.next({
-      ctx: {
-        customer
-      }
-    })
+export const customerProcedure = authedProcedure.use(async (opts) => {
+  const customer = await opts.ctx.prisma.customer.findUnique({
+    where: {
+      userId: opts.ctx.user.id,
+    },
   });
+
+  if (!customer) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: "No order found",
+    });
+  }
+
+  return opts.next({
+    ctx: {
+      customer,
+    },
+  });
+});
 
 export const userRouter = router({
   joinTable: authedProcedure
@@ -31,17 +31,45 @@ export const userRouter = router({
     .mutation(async ({ ctx, input }) => {
       return ctx.prisma.table.update({
         where: {
-          id: input.tableId
+          id: input.tableId,
         },
         data: {
           occupied: true,
           customers: {
             create: {
-              userId: ctx.user.id
-            }
-          }
-        }
-      })
+              userId: ctx.user.id,
+            },
+          },
+        },
+      });
+    }),
+  addLikes: authedProcedure
+    .input(z.object({ userId: z.string(), likes: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.user.update({
+        where: {
+          id: input.userId,
+        },
+        data: {
+          likes: {
+            push: input.likes,
+          },
+        },
+      });
+    }),
+  addDislikes: authedProcedure
+    .input(z.object({ userId: z.string(), dislikes: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.user.update({
+        where: {
+          id: input.userId,
+        },
+        data: {
+          dislikes: {
+            push: input.dislikes,
+          },
+        },
+      });
     }),
   addToOrder: customerProcedure
     .input(z.object({ foodId: z.string() }))
@@ -50,45 +78,45 @@ export const userRouter = router({
         where: {
           customers: {
             some: {
-              id: ctx.customer.id
-            }
-          }
-        }
+              id: ctx.customer.id,
+            },
+          },
+        },
       });
 
       if (!table) {
         throw new TRPCError({
           code: "NOT_FOUND",
-          message: "Table not found"
+          message: "Table not found",
         });
       }
 
       const food = await ctx.prisma.food.findUnique({
         where: {
           id: input.foodId,
-          restaurantId: table.restaurantId
-        }
+          restaurantId: table.restaurantId,
+        },
       });
 
       if (!food) {
         throw new TRPCError({
           code: "NOT_FOUND",
-          message: "Food not found at this restaurant"
+          message: "Food not found at this restaurant",
         });
       }
 
       return ctx.prisma.customer.update({
         where: {
-          id: ctx.customer.id
+          id: ctx.customer.id,
         },
         data: {
           foods: {
             create: {
-              foodId: input.foodId
-            }
-          }
-        }
-      })
+              foodId: input.foodId,
+            },
+          },
+        },
+      });
     }),
   removeFromOrder: customerProcedure
     .input(z.object({ foodId: z.string() }))
@@ -97,9 +125,9 @@ export const userRouter = router({
         where: {
           customerId_foodId: {
             customerId: ctx.customer.id,
-            foodId: input.foodId
-          }
-        }
-      })
-    })
+            foodId: input.foodId,
+          },
+        },
+      });
+    }),
 });
